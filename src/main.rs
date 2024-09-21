f358e0b217d9b63bb17e79a783cb0ac40cb7417a
penghs <penghs@agilean.cn>
2024-09-21 14:09:38 +0800

    使用tokio的事件驱动机制支持多个客户端

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,33 @@
-use std::{net::TcpListener, io::Write, thread};
-use std::io::Read;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+//Tokio提供了执行异步任务的运行时。大多数应用程序可以使用#[tokio::main]宏在tokio运行时运行它们的代码。
+//但是，这个宏只提供基本的配置选项。作为替代方案，tokio::runtime模块为配置和管理运行时提供了更强大的api。如果#[tokio::main]宏不提供所需的功能，则应该使用该模块。
+#[tokio::main]
+//async fn main() -> std::io::Result<()> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    //std::net::TcpListener是阻塞的，而tokio::net::TcpListener是异步的
+    let listener = TcpListener::bind("127.0.0.1:6379").await?; //使用?时，函数的返回值不能是()
     println!("Redis Server Started at 6379!");
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                thread::spawn(move || {
-                    let mut buf = [0; 1024];
-                    //使用循环等待新的命令
-                    loop {
-                        ////这个方法会阻塞，直到客户端输入命令
-                        let n = stream.read(&mut buf).unwrap();
-                        //直到收到 bye命令是才关闭连接
-                        if String::from_utf8_lossy(&buf[..n]).contains("bye") {
-                            break;
-                        }
-                        stream.write(b"+PONG\r\n").unwrap();
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            println!("Accept connection from {:?}", socket.peer_addr().unwrap());
+            let mut buf = [0; 1024];
+            loop {
+                match socket.read(&mut buf).await {
+                    Ok(n) if n == 0 => {
+                        println!("Connection from {} closed", socket.peer_addr().unwrap());
+                        return;
                     }
-                });
+                    Ok(n) => {
+                        if let Err(e) = socket.write(b"+PONG\r\n").await {
+                            eprintln!("Failed to send PONG!");
+                        }
+                    }
+                    Err(_) => {}
+                }
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        });
     }
 }
