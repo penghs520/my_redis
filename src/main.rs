5ffb1bf3b5078675b824cc56e22a29919622cf92
penghs <penghs@agilean.cn>
2024-09-21 10:33:09 +0800

    搭建tcp server，监听6379端口

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::net::TcpListener;
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    println!("Redis Server Started at 6379!");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                println!("accepted new connection");
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
 }
+
+
