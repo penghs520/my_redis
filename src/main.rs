042bc30ac1b27c18559fff3af314bde436fb4e78
penghs <penghs@agilean.cn>
2024-09-21 11:06:58 +0800

    在一次连接中发送多次命令

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::{
     net::TcpListener,
     io::Write,
 };
+use std::io::Read;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -11,7 +12,17 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                stream.write(b"+PONG\r\n").unwrap();
+                let mut buf = [0; 1024];
+                //使用循环等待新的命令
+                loop {
+                    ////这个方法会阻塞，直到客户端输入命令
+                    let n = stream.read(&mut buf).unwrap();
+                    //直到收到 bye命令是才关闭连接
+                    if String::from_utf8_lossy(&buf[..n]).contains("bye") {
+                        break;
+                    }
+                    stream.write(b"+PONG\r\n").unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -19,4 +30,3 @@ fn main() {
         }
     }
 }
-
